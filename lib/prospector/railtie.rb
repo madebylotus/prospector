ff0f0c58a18f068cc7b7cd75cf2293cc05f13116
Matthew Millsaps-Brewer <matt@madebylotus.com>
2015-08-19 20:17:50 -0400

    Enqueue via background on load of ActionController

--- lib/prospector/railtie.rb
+++ lib/prospector/railtie.rb
@@ -2,6 +2,7 @@ module Prospector
   class Railtie < Rails::Railtie
     initializer 'prospector.railtie' do |app|
       ActiveSupport.on_load :action_controller do |app|
+        Background.enqueue
       end
     end
   end
