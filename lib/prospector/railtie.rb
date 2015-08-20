7b23fc4bacfedbec621a66a60d06c79551372edf
Matthew Millsaps-Brewer <matt@madebylotus.com>
2015-08-20 07:05:16 -0400

    Skip loading Prospector in rake tasks

--- lib/prospector/railtie.rb
+++ lib/prospector/railtie.rb
@@ -1,7 +1,7 @@
 module Prospector
   class Railtie < Rails::Railtie
     config.after_initialize do
-      Background.enqueue
+      Background.enqueue unless Object.const_defined?('Rake')
     end
   end
 end
