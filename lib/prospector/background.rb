d9f6f3424f058f0b2a3cbcf310c052e97cce9f08
Matthew Millsaps-Brewer <matt@madebylotus.com>
2015-12-15 20:42:57 -0500

    Offer better detection when running in Rails server for auto-reporting
    Detecting if Rake constant was not reliable in determining if running
    via a Rails app server, running from the command line, or via a Rake
    task.
    
    Heavily borrowed from this SO answer with a great reference to the
    NewRelic ruby client: http://stackoverflow.com/a/12246652

--- lib/prospector/background.rb
+++ lib/prospector/background.rb
@@ -1,13 +1,22 @@
 module Prospector
   module Background
-    autoload :Coordinator,  'prospector/background/coordinator'
-    autoload :NotifyJob,    'prospector/background/notify_job'
-    autoload :NotifyWorker, 'prospector/background/notify_worker'
+    autoload :Coordinator,          'prospector/background/coordinator'
+    autoload :EnvironmentDetector,  'prospector/background/environment_detector'
+    autoload :NotifyJob,            'prospector/background/notify_job'
+    autoload :NotifyWorker,         'prospector/background/notify_worker'
+
+    module_function
 
     def enqueue
-      Coordinator.new.enqueue
+      service = Coordinator.new
+
+      service.enqueue
     end
 
-    module_function :enqueue
+    def auto_reporting_enabled?
+      service = EnvironmentDetector.new
+
+      service.rails_server?
+    end
   end
 end
